57a2d255c81a1836bed53a0808d2ef8396784ccc
sbsteacher <id@gmail.com>
2025-09-12 16:44:05 +0900

    도커파일 업로드

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { useAuthenticationStore } from '@/stores/authentication';
 
 const router = createRouter({
   //history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/2025-01-fe-greengram/' : import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
