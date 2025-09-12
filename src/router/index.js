8eaae5276cae64050c6a6a44a73f26ae585b3a75
sbsteacher <id@gmail.com>
2025-09-12 12:44:26 +0900

    경로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthenticationStore } from '@/stores/authentication';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/2025-01-fe-greengram/' : import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
